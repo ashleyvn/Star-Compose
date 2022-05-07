6b1e376b2b39366b35fea029e68976ccd64fccd2
ashleyvn <ashleyvn@vt.edu>
2022-05-07 12:50:08 -0400

    icons

--- star-compose/src/app/menubar/menubar.component.ts
+++ star-compose/src/app/menubar/menubar.component.ts
@@ -24,49 +24,13 @@ export class MenubarComponent implements OnInit {
       this.playStatus = "Stop";
       this.buttonPressed.emit(true);
       this.renderAudio.emit(true);
+      this.hideIcons("hidden")
     }
     else if (this.playStatus === "Stop") {
       this.playStatus = "Play"
       this.buttonPressed.emit(false);
       this.renderAudio.emit(false);
-    }
-
-    //Disable Buttons
-    let constellationButton = document.getElementsByClassName("starButton")
-    constellationButton[0].setAttribute("disabled", "")
-    
-    //Close Menu Bar
-    this.navbarOpened = false
-
-    //Hide Icons
-    let icons = [];
-    icons.push(document.getElementsByClassName("outside"))
-    icons.push(document.getElementsByClassName("drag-icon"))
-    icons.push(document.getElementsByClassName("rotate-icon"))
-    icons.push(document.getElementsByClassName("delete-icon"))
-    icons.push(document.getElementsByClassName("resize-grip"))
-    icons.push(document.getElementsByClassName("constellation-name"))
-    icons.push(document.getElementsByClassName("custom-range  my-2"))
-
-    if (icons[0] != null) {
-      for (var i = 0; i < icons.length; i++) {
-        for (var j = 0; j < icons[i].length; j++) {
-          if (i == 0) {
-            var str = icons[i][j].getAttribute("style")
-            if ( str == null) {
-              str = ""
-            }
-            icons[i][j].setAttribute("style", str + "border: 2px solid transparent;")
-          }
-          else {
-            var str = icons[i][j].getAttribute("style")
-            if ( str == null) {
-              str = ""
-            }
-            icons[i][j].setAttribute("style", str + "visibility: hidden;")
-          }
-        }
-      }
+      this.hideIcons("visible")
     }
   }
 
@@ -100,6 +64,52 @@ export class MenubarComponent implements OnInit {
   
   toggleNavbar() {
     this.navbarOpened = !this.navbarOpened;
+  }
+
+  hideIcons(visibility:string) {
+    //Disable Buttons
+    let constellationButton = document.getElementsByClassName("starButton")
+    constellationButton[0].setAttribute("disabled", "")
+
+    //Close Menu Bar
+    this.navbarOpened = false
+
+    //Hide Icons
+    let icons = [];
+    icons.push(document.getElementsByClassName("outside"))
+    icons.push(document.getElementsByClassName("drag-icon"))
+    icons.push(document.getElementsByClassName("rotate-icon"))
+    icons.push(document.getElementsByClassName("delete-icon"))
+    icons.push(document.getElementsByClassName("resize-grip"))
+    icons.push(document.getElementsByClassName("constellation-name"))
+    icons.push(document.getElementsByClassName("custom-range  my-2"))
+
+    if (icons[0] != null) {
+      for (var i = 0; i < icons.length; i++) {
+        for (var j = 0; j < icons[i].length; j++) {
+          if (i == 0) {
+            var str = icons[i][j].getAttribute("style")
+            if ( str == null) {
+              str = ""
+            }
+            if (visibility == "hidden")
+            {
+              icons[i][j].setAttribute("style", str + "border: solid 2px transparent;")
+            }
+            else {
+              icons[i][j].setAttribute("style", str + "border: dotted #ccc 2px;")
+            }
+          }
+          else {
+            var str = icons[i][j].getAttribute("style")
+            if ( str == null) {
+              str = ""
+            }
+            icons[i][j].setAttribute("style", str + "visibility: "+visibility+ ";")
+          }
+        }
+      }
+    }
   }
 
 }
